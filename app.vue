eccd64fa5b0cc0db7796b13493d6cbd8ad63893e
Paul Barrett <paulbarrett79@gmail.com>
2025-09-03 12:09:45 +0100

    neater links, experience boxes

--- app.vue
+++ app.vue
@@ -1,94 +1,106 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from '@nuxt/ui'
-const open = ref(false)
+import type { NavigationMenuItem } from "@nuxt/ui";
+const open = ref(false);
 
 const items = ref<NavigationMenuItem[]>([
   {
-    label: 'Case studies',
-    icon: 'i-lucide-notebook', 
-    to: '/studies',
-    onSelect(e) {
-      open.value = false
+    label: "Case studies",
+    icon: "i-lucide-notebook",
+    to: "/studies",
+    onSelect() {
+      open.value = false;
     },
   },
   {
-    label: 'About me',
-    icon: 'i-lucide-user',
-    to: '/about',
-    onSelect(e) {
-      open.value = false
+    label: "About me",
+    icon: "i-lucide-user",
+    to: "/about",
+    onSelect() {
+      open.value = false;
     },
   },
   {
-    label: 'Download CV',
-    icon: 'i-lucide-cloud-download',
-    to: '/paulbarrettcouk_cv.pdf',
-    target: '_blank',
-    onSelect(e) {
-      open.value = false
+    label: "Download CV",
+    icon: "i-lucide-cloud-download",
+    to: "/paulbarrettcouk_cv.pdf",
+    target: "_blank",
+    onSelect() {
+      open.value = false;
     },
   },
   {
-    label: 'Github',
-    icon: 'i-simple-icons-github', 
-    to: 'https://github.com/paulbarrett79',
-    target: '_blank',
-    onSelect(e) {
-      open.value = false
+    label: "Github",
+    icon: "i-simple-icons-github",
+    to: "https://github.com/paulbarrett79",
+    target: "_blank",
+    onSelect() {
+      open.value = false;
     },
   },
   {
-    label: 'LinkedIn',
-    icon: 'i-simple-icons-linkedin',
-    to: 'https://www.linkedin.com/in/paul-barrett-89040426/',
-    target: '_blank',
-    onSelect(e) {
-      open.value = false
+    label: "LinkedIn",
+    icon: "i-simple-icons-linkedin",
+    to: "https://www.linkedin.com/in/paul-barrett-89040426/",
+    target: "_blank",
+    onSelect() {
+      open.value = false;
     },
-  }
-])
+  },
+]);
 </script>
 
 <template>
-    <UApp class="flex grow h-full">
-      <UContainer class="md:w-full h-full flex flex-col justify-stretch">
-        <header class="flex justify-between items-center">
-          <NuxtLink to="/">
-            <NuxtImg src="/img/pblogodark.png" height="50px" width="auto" alt="PB logo" ></NuxtImg>
-          </NuxtLink>
-          
-          <UNavigationMenu :items="items" class="hidden md:flex w-full justify-end align-center" />
-          <USlideover 
-            direction="right" 
-            class="md:hidden" 
-            title="Menu"
-            v-model:open="open"
-            :close="{
-              color: 'neutral',
-              variant: 'outline',
-              class: 'rounded-full'
-            }">
-            <UButton size="xl" icon="i-lucide-menu" variant="outline" color="neutral"></UButton>
-            <template #body>
-              <UNavigationMenu :items="items" orientation="vertical" class="justify-start align-start" />
-            </template>
-          </USlideover>
-        </header>
-        <div class="blue-container flex rounded-[20px] md:rounded-[60px] gap-0 md:gap-1">
-          <nav class="lhs shrink md:max-w-10 flex justify-center">
-            
-              <NuxtImg src="/img/tall_profile.jpg" class="max-h-[100px] max-w-fit md:max-h-none rounded-[5px] md:rounded-[40px]" alt="Picture of Paul wearing sunglasses" ></NuxtImg>
-            
-          </nav>
-          <section class="rhs flex flex-col grow">
-            <NuxtPage />
-          </section>
-        </div>
-        <footer class="p-10 text-center">
-          <small class="text-gray-400">Paul Barrett 2025. Site built with Nuxt and hosted on Github Pages.</small>
-        </footer>
-      </UContainer>
-    </UApp>
+  <UApp class="flex grow h-full">
+    <UContainer class="md:w-full h-full flex flex-col justify-stretch">
+      <header class="flex justify-between items-center">
+        <NuxtLink to="/">
+          <NuxtImg src="/img/pblogodark.png" height="50px" width="auto" alt="PB logo" />
+        </NuxtLink>
+
+        <UNavigationMenu
+          :items="items"
+          class="hidden md:flex w-full justify-end align-center"
+        />
+        <USlideover
+          v-model:open="open"
+          direction="right"
+          class="md:hidden"
+          title="Menu"
+          :close="{
+            color: 'neutral',
+            variant: 'outline',
+            class: 'rounded-full',
+          }"
+        >
+          <UButton size="xl" icon="i-lucide-menu" variant="outline" color="neutral" />
+          <template #body>
+            <UNavigationMenu
+              :items="items"
+              orientation="vertical"
+              class="justify-start align-start"
+            />
+          </template>
+        </USlideover>
+      </header>
+      <div class="blue-container flex rounded-[20px] md:rounded-[60px] gap-0 md:gap-1">
+        <nav class="lhs shrink md:max-w-10 flex justify-center">
+          <NuxtImg
+            src="/img/tall_profile.jpg"
+            class="max-h-[100px] max-w-fit md:max-h-none rounded-[5px] md:rounded-[40px]"
+            alt="Picture of Paul wearing sunglasses"
+          />
+        </nav>
+        <section class="rhs flex flex-col grow">
+          <NuxtPage />
+        </section>
+      </div>
+      <footer class="p-10 text-center">
+        <small class="text-gray-400"
+          >Paul Barrett 2025. Site built with Nuxt and hosted on Github Pages.</small
+        >
+      </footer>
+    </UContainer>
+  </UApp>
 </template>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,23 +3,58 @@
 <template>
   <div class="flex flex-col">
     <h1 class="px-4">
-      Hi my name is Paul Barrett and I have been doing things with websites since 2006.
+      Hi my name is Paul Barrett and this is my portfolio site. Welcome!
     </h1>
     <h2 class="px-4 mt-2 md:mt-20">
       I’ve been a full stack dev, front end developer, UX developer, UX designer and loads
       of other things
     </h2>
-    <p class="px-4 mt-2 md:mt-10">I have a lot of experience with:</p>
-    <div class="px-4 mt-2">
-      <ul class="px-4">
-        <li>Wireframing and ideation in Miro and Figjam (and paper!)</li>
-        <li>Prototyping and design system construction in Figma</li>
-        <li>
-          Front end development in Vue/Nuxt - which I used to build this site! - as well
-          as React, good old classic HTML and loads of CSS experience
-        </li>
-      </ul>
-    </div>
+    <section id="experience" class="flex justify-between p-4">
+      <UCard class="m-2 text-center content-between">
+        <div class="flex grow justify-center mb-4">
+          <UIcon name="i-lucide-notebook" class="text-black custom-iconify" />
+          <UIcon name="i-lucide-pencil" class="text-black custom-iconify" />
+        </div>
+        <div class="flex shrink">
+          <span class="text-gray-700 text-sm"
+            >Wireframing and ideation in Miro and Figjam (and paper!)</span
+          >
+        </div>
+      </UCard>
+      <UCard class="m-2 text-center content-between text-xl">
+        <div class="flex grow justify-center mb-4">
+          <UIcon name="i-lucide-figma" class="text-black custom-iconify" />
+          <UIcon name="i-lucide-workflow" class="text-black custom-iconify" />
+        </div>
+        <div class="flex shrink">
+          <span class="text-gray-700 text-sm"
+            >Prototyping and design system construction in Figma</span
+          >
+        </div>
+      </UCard>
+      <UCard class="m-2 text-center content-end">
+        <div class="flex grow justify-center mb-4">
+          <UIcon name="i-lucide-terminal" class="text-black custom-iconify" />
+          <UIcon name="i-lucide-braces" class="text-black custom-iconify" />
+        </div>
+        <div class="flex shrink">
+          <span class="text-gray-700 text-sm"
+            >Front end development in Vue/Nuxt as well as React</span
+          >
+        </div>
+      </UCard>
+      <UCard class="m-2 text-center content-end">
+        <div class="flex grow justify-center mb-4">
+          <UIcon name="i-lucide-code-xml" class="text-black custom-iconify" />
+          <UIcon name="i-lucide-paint-roller" class="text-black custom-iconify" />
+        </div>
+        <div class="flex shrink">
+          <span class="text-gray-700 text-sm"
+            >Good old classic HTML and loads of CSS experience</span
+          >
+        </div>
+      </UCard>
+    </section>
     <p class="px-4 mt-2">
       I have worn quite a few hats - UX design, interaction design, wireframing and
       prototyping, UX research and reporting directly to stakeholders as well as
@@ -28,18 +63,19 @@
       ecosystem.
     </p>
     <div class="flex px-4 mt-4">
-      <UButton
-        size="xl"
-        color="secondary"
-        to="/studies"
-        icon="i-lucide-notebook"
-        class="mr-2"
-      >
-        <span class="font-bold">Case Studies</span>
-      </UButton>
-      <UButton size="xl" color="secondary" to="/about" icon="i-lucide-user" class="mr-2">
-        <span class="font-bold">About Me</span>
-      </UButton>
+      <NuxtLink color="secondary" to="/studies" class="mr-4 px-2 flex items-center">
+        <UIcon name="i-lucide-notebook" /><span class="font-bold">Case Studies</span>
+      </NuxtLink>
+      <NuxtLink color="secondary" to="/about" class="mr-2 px-2 flex items-center">
+        <UIcon name="i-lucide-user" /><span class="font-bold">About me</span>
+      </NuxtLink>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+.custom-iconify {
+  width: 40px;
+  height: 40px;
+}
+</style>
