164e763d0d7a963eb4f984df742935f8dd7304f2
Paul Barrett <paulbarrett79@gmail.com>
2025-09-11 13:46:26 +0100

    better hover effect

--- components/BlogPostThumb.vue
+++ components/BlogPostThumb.vue
@@ -1,42 +1,45 @@
 <template>
-    <div class="post-container flex flex-col">
-        <div class="post-thumb-img align-stretch">
-            <slot name="thumb" />
-        </div>
-        <div class="post-thumb-text aligns-stretch">
-            <h2><slot name="title"/></h2>
-            <h3 class="hidden md:flex"><slot name="subtitle" /></h3>
-        </div>
+  <div class="post-container flex flex-col">
+    <div class="post-thumb-img align-stretch">
+      <slot name="thumb" />
     </div>
+    <div class="post-thumb-text aligns-stretch">
+      <h2><slot name="title" /></h2>
+      <h3 class="hidden md:flex"><slot name="subtitle" /></h3>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 .post-container {
+  overflow-x: hidden;
+  .post-thumb-text {
+    background: #030c3f;
+    padding: 20px;
+    transform: translateY(-50px);
+    transition: all 0.2s ease-out;
+    h2,
+    h3 {
+      color: lightgray;
+    }
+  }
+  .post-thumb-img {
+    opacity: 0.6;
+    transform: scale(100%);
+    transition: all 0.2s ease-out;
+  }
+  &:hover {
     .post-thumb-text {
-        background: #030C3F;
-        padding: 20px;
-        transform: translateY(-50px);
-        transition: all 0.2s ease-out;
-        h2, h3 {
-            color: lightgray
-        }
+      background: #03607c;
+      h2,
+      h3 {
+        color: unset;
+      }
     }
     .post-thumb-img {
-        opacity: 0.6;
-        transition: all 0.2s ease-out;
+      opacity: 1;
+      transform: scale(110%);
     }
-    &:hover {
-        .post-thumb-text {
-            background: #03607C;
-            h2, h3 {
-                color: unset;
-            }
-        }
-        .post-thumb-img {
-            opacity: 1;
-        }
-    }
+  }
 }
-
-
 </style>
