e460da0e205cf636bba6e9201fe36188d16ab765
Paul Barrett <paulbarrett79@gmail.com>
2025-11-04 15:36:48 +0000

    nice transition on case studies index

--- pages/studies/index.vue
+++ pages/studies/index.vue
@@ -9,7 +9,7 @@ const { data: studies } = await useAsyncData("studies", () =>
     <h1 class="pl-10">Here are my case studies.</h1>
     <div class="pl-10 sm:w-[400px] md:w-[800px]">
       <ul class="pt-10">
-        <li v-for="post in studies" :key="post.id">
+        <li v-for="post in studies" :key="post.id" class="cardycard">
           <NuxtLink :to="post.path" class="ingore-link-animations">
             <BlogPostThumb>
               <template #thumb>
@@ -28,3 +28,32 @@ const { data: studies } = await useAsyncData("studies", () =>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+  .cardycard {
+    opacity: 1;
+    transition: all 0.6s ease-in-out;
+    &:nth-child(1) {
+      transition-delay: 0.4s;
+    }
+    &:nth-child(2) {
+      transition-delay: 0.8s;
+    }
+    &:nth-child(3) {
+      transition-delay: 1.2s;
+    }
+    &:nth-child(4) {
+      transition-delay: 1.6s;
+    }
+  }
+}
+.page-enter-from,
+.page-leave-to {
+  .cardycard {
+    opacity: 0;
+  }
+  filter: blur(1rem);
+}
+</style>
